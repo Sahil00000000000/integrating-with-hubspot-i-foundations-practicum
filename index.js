2285455117cc0d5ba1225ca939553e7a55ed64e4
Sahil00000000000 <sahilimran5july@gmail.com>
2024-09-09 23:03:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+require('dotenv').config();
+
+
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
